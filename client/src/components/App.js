a4594e739ad25e240168919072343f927daba5a4
William <will14lee@gmail.com>
2022-01-04 21:13:05 -0500

    Add routes for webpage

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -19,18 +19,53 @@ function App() {
       <div className="App">
         <h1>Fic. E!</h1>
         <Switch>
-          <Route path="/testing">
-            <h1>Test Route</h1>
-          </Route>
-          <Route path="/login">
-            <LoginForm/>
-          </Route>
-          <Route path= "/signup">
-            <SignUpForm/>
-          </Route>
-          <Route path="/">
-            <Home/>
-          </Route>
+          <Route path="/login">  <LoginForm/>  </Route>
+
+          <Route path= "/signup">  <SignUpForm/>  </Route>
+
+          <Route path="/">  <Home/>  </Route>
+
+          <Route path= "/new">  </Route>  
+          
+          <Route path= "/details">  </Route>
+
+          <Route path= "/edit">  </Route>
+
+          <Route path= "/:id/chapters">  </Route>
+
+          <Route path= "/:id/chapters/new">  </Route>
+
+          <Route path= "/:id/chapters/edit">  </Route>
+          
+          <Route path= "/:story_id/chapters/:id/pages">  </Route>
+
+          <Route path= "/:story_id/chapters/:chapter_id/pages/:id">   </Route>
+
+          <Route path= "/:story_id/chapters/:chapter_id/pages/:id/new">  </Route>
+
+          <Route path= "/:story_id/chapters/:id/pages/new">  </Route>
+          
+          <Route path= "/:story_id/chapters/:id/pages/new">  </Route>
+
+          <Route path= "/:story_id/chapters/:id/pages/edit">  </Route>
+
+          <Route path= "/stories">  </Route>
+          
+          <Route path= "/stories/:username/chapters/details">  </Route>
+          
+          <Route path= "/stories/:username/chapters/:id/pages">  </Route>
+          
+          <Route path= "/stories/:username/chapters/:chapter_id/pages/:id">  </Route>
+
+          <Route path= "/original_chapter/:id">  </Route>
+
+          <Route path= "/original_chapter/:id/new">  </Route>
+
+          <Route path= "/original_chapter/:id/pages/new">  </Route>
+
+          <Route path= "/original_chapter/:chapter_id/pages/:id/edit">  </Route>
+
+          <Route path= "/original_chapter/:chapter_id/pages/:id/edit">  </Route>
         </Switch>
       </div>
     </BrowserRouter>
