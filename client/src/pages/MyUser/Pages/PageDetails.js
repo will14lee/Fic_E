c0989a0f86b6ce4f77780a5a0f0aa20573a81e75
William <will14lee@gmail.com>
2022-01-17 06:43:06 -0500

    Fix

--- client/src/pages/MyUser/Pages/PageDetails.js
+++ client/src/pages/MyUser/Pages/PageDetails.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useHistory, useParams, Link } from 'react-router-dom'
 
 const PageDetails = () => {
-
+    const params=useParams()
+    const history=useHistory()
     function handleDelete(){
         console.log("Delete")
         fetch(`/stories/${params.story_id}/chapters/${params.chapter_id}/pages`, {
